5658b7fd5779ed6b228e899eaa9129a7f7b6a2ca
Kyoichiro Yamada <me@kyoh86.dev>
2021-07-22 18:51:31 +0900

    use plenary.strings.strdisplaywidth instead of telescope.utils.strdisplaywidth

--- lua/telescope/_extensions/gogh_repos.lua
+++ lua/telescope/_extensions/gogh_repos.lua
@@ -1,10 +1,11 @@
-local actions = require'telescope.actions'
-local conf = require'telescope.config'.values
-local entry_display = require'telescope.pickers.entry_display'
-local finders = require'telescope.finders'
-local pickers = require'telescope.pickers'
-local utils = require'telescope.utils'
-local gogh_a = require'telescope._extensions.gogh_actions'
+local actions = require "telescope.actions"
+local conf = require "telescope.config".values
+local entry_display = require "telescope.pickers.entry_display"
+local finders = require "telescope.finders"
+local pickers = require "telescope.pickers"
+local utils = require "telescope.utils"
+local gogh_a = require "telescope._extensions.gogh_actions"
+local strings = require "plenary.strings"
 
 local M = {}
 
@@ -14,32 +15,41 @@ M.repos = function(opts)
   local results = {}
   local widths = {
     rel = 0,
-    attr = 0,
+    attr = 0
   }
 
   local parse_line = function(line)
     local entry = vim.fn.json_decode(line)
-    entry.rel = entry.spec.host .. '/' .. entry.spec.owner .. '/' .. entry.spec.name
+    entry.rel = entry.spec.host .. "/" .. entry.spec.owner .. "/" .. entry.spec.name
     local attr = {}
     if entry.private then
-      table.insert(attr, 'private')
+      table.insert(attr, "private")
     end
     if entry.fork then
-      table.insert(attr, 'fork')
+      table.insert(attr, "fork")
     end
     if entry.archived then
-      table.insert(attr, 'archived')
+      table.insert(attr, "archived")
     end
-    entry.attr = table.concat(attr, ',')
+    entry.attr = table.concat(attr, ",")
     for key, value in pairs(widths) do
-      widths[key] = math.max(value, utils.strdisplaywidth(entry[key] or ''))
+      widths[key] = math.max(value, strings.strdisplaywidth(entry[key] or ""))
     end
     table.insert(results, entry)
   end
 
-  local output = utils.get_os_command_output({
-    opts.bin, 'repos', '--format', 'json', '--limit', '0',
-  }, opts.cwd)
+  local output =
+    utils.get_os_command_output(
+    {
+      opts.bin,
+      "repos",
+      "--format",
+      "json",
+      "--limit",
+      "0"
+    },
+    opts.cwd
+  )
   for _, line in ipairs(output) do
     parse_line(line)
   end
@@ -47,50 +57,54 @@ M.repos = function(opts)
     return
   end
 
-  local displayer = entry_display.create{
+  local displayer =
+    entry_display.create {
     separator = " ",
     items = {
       {width = widths.rel},
       {width = widths.attr},
-      {remaing = true},
-    },
+      {remaing = true}
+    }
   }
 
   local make_display = function(entry)
-    return displayer{
+    return displayer {
       {entry.rel, "TelescopeResultsIdentifier"},
       {entry.attr},
-      {entry.description},
+      {entry.description}
     }
   end
 
-  pickers.new(opts, {
-    prompt_title = 'Repositories on GitHub',
-    finder = finders.new_table {
-      results = results,
-      entry_maker = function(entry)
-        entry.value = entry.url
-        entry.ordinal = entry.url
-        entry.display = make_display
-        return entry
-      end,
-    },
-    sorter = conf.file_sorter(opts),
-    attach_mappings = function(_,map)
-      ckeys = opts['keys']['repos']
-      for op, key in next, ckeys do
-        act = gogh_a[op]
-        if key ~= nil then
-          if key == 'default' then
-            actions.select_default:replace(act)
-          else
-            map('i', key, act)
+  pickers.new(
+    opts,
+    {
+      prompt_title = "Repositories on GitHub",
+      finder = finders.new_table {
+        results = results,
+        entry_maker = function(entry)
+          entry.value = entry.url
+          entry.ordinal = entry.url
+          entry.display = make_display
+          return entry
+        end
+      },
+      sorter = conf.file_sorter(opts),
+      attach_mappings = function(_, map)
+        ckeys = opts["keys"]["repos"]
+        for op, key in next, ckeys do
+          act = gogh_a[op]
+          if key ~= nil then
+            if key == "default" then
+              actions.select_default:replace(act)
+            else
+              map("i", key, act)
+            end
           end
         end
+        return true
       end
-      return true
-    end,
-  }):find()
+    }
+  ):find()
 end
 
 return M
