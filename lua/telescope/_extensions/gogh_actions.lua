7f8a301d72af28782c1f702889aeb9ab570d6ba1
Kyoichiro Yamada <me@kyoh86.dev>
2022-05-10 22:23:10 +0900

    use vim.notify instead of print

--- lua/telescope/_extensions/gogh_actions.lua
+++ lua/telescope/_extensions/gogh_actions.lua
@@ -5,51 +5,51 @@ local from_entry = require("telescope.from_entry")
 local A = {}
 
 local function close_telescope_prompt(prompt_bufnr)
-  local entry = actions_state.get_selected_entry(prompt_bufnr)
-  actions.close(prompt_bufnr)
-  return from_entry.path(entry)
+    local entry = actions_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    return from_entry.path(entry)
 end
 
 -- open the target project
 A.open = function(prompt_bufnr)
-  local dir = close_telescope_prompt(prompt_bufnr)
-  require("telescope.builtin").git_files({ cwd = dir })
+    local dir = close_telescope_prompt(prompt_bufnr)
+    require("telescope.builtin").git_files({ cwd = dir })
 end
 
 -- browse the target repository
 A.browse = function(prompt_bufnr)
-  local entry = actions_state.get_selected_entry(prompt_bufnr)
-  actions.close(prompt_bufnr)
-  vim.fn["openbrowser#open"](entry.url)
+    local entry = actions_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    vim.fn["openbrowser#open"](entry.url)
 end
 
 -- global chdir
 A.cd = function(prompt_bufnr)
-  local dir = close_telescope_prompt(prompt_bufnr)
-  vim.cmd("cd " .. dir)
-  print("chdir to " .. dir)
+    local dir = close_telescope_prompt(prompt_bufnr)
+    vim.cmd("cd " .. dir)
+    vim.notify("chdir to " .. dir, vim.log.levels.INFO)
 end
 
 -- buffer local chdir
 A.lcd = function(prompt_bufnr)
-  local dir = close_telescope_prompt(prompt_bufnr)
-  vim.cmd("lcd " .. dir)
-  print("chdir buffer local to " .. dir)
+    local dir = close_telescope_prompt(prompt_bufnr)
+    vim.cmd("lcd " .. dir)
+    vim.notify("chdir buffer local to " .. dir, vim.log.levels.INFO)
 end
 
 -- tab local chdir
 A.tcd = function(prompt_bufnr)
-  local dir = close_telescope_prompt(prompt_bufnr)
-  vim.cmd("tcd " .. dir)
-  print("chdir tab to " .. dir)
+    local dir = close_telescope_prompt(prompt_bufnr)
+    vim.cmd("tcd " .. dir)
+    vim.notify("chdir tab to " .. dir, vim.log.levels.INFO)
 end
 
 -- get repository
 A.get = function(prompt_bufnr)
-  local entry = actions_state.get_selected_entry(prompt_bufnr)
-  actions.close(prompt_bufnr)
-  print("getting " .. entry.url)
-  vim.cmd("!gogh get " .. entry.rel)
+    local entry = actions_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    vim.notify("getting " .. entry.url)
+    vim.cmd("!gogh get " .. entry.rel, vim.log.levels.INFO)
 end
 
 return A
